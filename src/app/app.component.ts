c05f21f95b59e7d4732389723d2afe12a4db4c2f
pavi <paviguna116@gmail.com>
2024-09-14 16:45:41 +0530

    Angular standalone and version 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+// import { provideHttpClient } from '@angular/common/http';
+// import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { UpperCasePipe } from '@angular/common';
+import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UpperCasePipe, LowerCasePipe, TasksComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'youtube-ang';
+  
 }
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service';
+import { TaskItemComponent } from '../task-item/task-item.component';
+import {Task} from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [CommonModule, TaskItemComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService){
+
+  }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+}
